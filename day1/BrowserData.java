648e189e9e197cbfcab64fc7a5ab33419e9f1b42
Santhakumar143 <ksantha726@gmail.com>
2024-11-15 21:22:35 +0530

    Add files via upload
    day1

---
+++ day1/BrowserData.java
@@ -0,0 +1,20 @@
+package week1.day1;
+
+public class BrowserData {
+
+	public static void main(String[] args) {
+
+		int countTabs=4;
+		boolean apploaded=true;
+		char firstLetter='C';
+		String browserName="Chrome";
+		float browserVersion=212.53F;
+		long ph=8610822125l;
+		
+		System.out.println("Number tabs is open in the browser: "+countTabs);
+		System.out.println("Check whether the application is loaded: "+apploaded);
+		System.out.println(""+browserName);
+
+	}
+
+}
---
+++ day1/LearnIfElse.java
@@ -0,0 +1,26 @@
+package week1.day1;
+
+public class LearnIfElse {
+
+	public static void main(String[] args) {
+
+		int num=0;
+		
+		if (num>0) {
+			
+			System.out.println("Positive Number: "+num);
+		}
+		
+		else if (num==0) {
+			
+			System.out.println("Number is Neutral: "+num);
+		}
+
+		else {
+			
+			System.out.println("Negative Number: "+num);
+			
+		}
+	}
+
+}
